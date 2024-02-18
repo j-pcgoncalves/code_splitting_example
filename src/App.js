e3fbf5a1538f447846a8113425d3c42dff0005fe
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-02-18 17:07:32 +0000

    dynamic import

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import Page1 from './components/Page1';
-import Page2 from './components/Page2';
-import Page3 from './components/Page3';
 import './App.css';
 
 function App() {
   const [route, setRoute] = useState("page1");
+  const [Component, setComponent] = useState("");
 
   const onRouteChange = (route) => {
-    setRoute(route);
+    if (route === "page1") {
+      setRoute(route);
+    } else if (route === "page2") {
+      import("./components/Page2").then((Page2) => {
+        setRoute(route);
+        setComponent(Page2.default);
+      });
+    } else if (route === "page3") {
+      import("./components/Page3").then((Page3) => {
+        setRoute(route);
+        setComponent(Page3.default);
+      });
+    }
   };
 
   if (route === "page1") {
     return <Page1 onRouteChange={onRouteChange} />;
-  } else if (route === "page2") {
-    return <Page2 onRouteChange={onRouteChange} />;
-  } else if (route === "page3") {
-    return <Page3 onRouteChange={onRouteChange} />;
+  } else {
+    return Component;
   }
 }
 
