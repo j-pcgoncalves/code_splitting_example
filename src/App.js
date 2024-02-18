4454d7651b14a165ff50ff718ec264362a305dd8
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-02-18 16:07:53 +0000

    create Page1 Component and create route state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
+import Page1 from './components/Page1';
 import './App.css';
 
 function App() {
+  const [route, setRoute] = useState("page1");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Page1 />
+      {/* <Page2 />
+      <Page3 /> */}
     </div>
   );
 }
